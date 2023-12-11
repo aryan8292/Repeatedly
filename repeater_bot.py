1c19c8bb18a9ccc5a17206d2ad504c28c7fd87e4
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-12-11 17:20:10 +0530

    Create repeater_bot.py

---
+++ repeater_bot.py
@@ -0,0 +1,32 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import schedule
+import time
+
+# Replace 'YOUR_TOKEN' with your actual bot token
+TOKEN = 'YOUR_TOKEN'
+CHAT_ID = 'YOUR_CHAT_ID'  # Replace with your group chat ID
+
+def send_repeated_message(context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=CHAT_ID, text="Your repeated message here.")
+
+def start_repeated(update: Update, context: CallbackContext) -> None:
+    # Schedule the message to be sent every minute
+    schedule.every(1).minutes.do(send_repeated_message, context=context)
+
+def main() -> None:
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+
+    # Register the command
+    dispatcher.add_handler(CommandHandler("start_repeated", start_repeated))
+
+    updater.start_polling()
+
+    # Start the schedule
+    schedule.run_continuously()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
